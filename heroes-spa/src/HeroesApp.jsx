1dff053665e8f39172600909472b829e95303bdc
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2022-10-02 09:26:55 +0200

    Firts Commit

---
+++ heroes-spa/src/HeroesApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HeroesApp = () => {
+  return (
+    <div class="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+    </div>
+  )
+}
