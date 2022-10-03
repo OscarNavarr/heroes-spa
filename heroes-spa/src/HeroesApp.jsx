91e05282dc0e5be6b33205fdafe1184a03e02ba4
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2022-10-03 19:36:07 +0200

    React Router was add

--- heroes-spa/src/HeroesApp.jsx
+++ heroes-spa/src/HeroesApp.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { AppRouter } from './router/AppRouter'
 
 export const HeroesApp = () => {
   return (
-    <div class="alert alert-primary" role="alert">
-        A simple primary alert—check it out!
-    </div>
+    <>
+      <AppRouter/>
+    </>
   )
 }
